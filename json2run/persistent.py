4cebc143065879aded741206e109ed855a6d6b3a
Mark Lawrenson <mark@lawrenson.id.au>
2017-04-06 10:44:52 +1000

    Fix pymongo3 support.

--- json2run/persistent.py
+++ json2run/persistent.py
@@ -1,5 +1,5 @@
 from pymongo.database import Database
-from pymongo.connection import Connection
+from pymongo.mongo_client import MongoClient
 from threading import Lock
 from bson.objectid import ObjectId
 import sys
@@ -18,16 +18,16 @@ def synchronized(fun):
 
 class Persistent(object):
     """An object which can be saved on the database."""
-    
+
     connection = None
     """Current connection to database daemon."""
-    
+
     database = None
     """Current database."""
-    
+
     scm = None
     """SCM used for the codebase."""
-    
+
     config = {
         "host": "localhost",
         "port": 27017,
@@ -36,12 +36,12 @@ class Persistent(object):
         "database": "j2r"
     }
     """Default values for connection."""
-    
+
     def __init__(self, inner = None):
         """Initialize with empty object."""
-        
+
         self.sync = Lock()
-        
+
         if inner != None:
             self.inner = inner
         else:
@@ -49,12 +49,12 @@ class Persistent(object):
 
     def lock(self):
         """Acquire lock."""
-        self.sync.acquire()    
-        
+        self.sync.acquire()
+
     def unlock(self):
         """Release lock."""
         self.sync.release()
-    
+
     def __getitem__(self, field):
         """Subscript getter."""
         try:
@@ -68,27 +68,27 @@ class Persistent(object):
                 # not an index
                 raise KeyError
             except IndexError:
-                # no more elements 
+                # no more elements
                 raise StopIteration
-            
+
     def __setitem__(self, field, value):
         """Subscript setter."""
         self.inner[field] = value
-        
+
     def __contains__(self, field):
         """Checks whether container has item."""
         return field in self.inner
 
     def save(self):
         """Upsert inner object."""
-        
+
         database = Persistent.database
         try:
             if "_id" not in self:
                 self["user"] = Persistent.user()
                 self["host"] = Persistent.host()
                 self["system"] = Persistent.platform()
-        
+
             self["_id"] = database[self.collection()].save(self.inner)
         except Exception as e:
             print "Failed saving on database: ", e
@@ -101,28 +101,28 @@ class Persistent(object):
     @classmethod
     def get(cls, query, fields = None):
         """Get matching persistents from database."""
-        
+
         database = Persistent.database
         return database[cls.collection()].find(query, fields)
-    
+
     @classmethod
     def remove(cls, query):
         """Remove matching persistents from database."""
-        
+
         database = Persistent.database
         database[cls.collection()].remove(query)
-    
+
     @staticmethod
     def connect(**kwargs):
         """Connect to the database with the specified data."""
-        
+
         config = kwargs
         config["pass"] = kwargs["passw"]
         del(config["passw"])
         Persistent.config = config
-            
+
         try:
-            Persistent.connection = Connection(config["host"], config["port"])
+            Persistent.connection = MongoClient(config["host"], config["port"])
             Persistent.database = Persistent.connection[config["database"]]
             Persistent.database.authenticate(config["user"], config["pass"])
         except Exception, e:
@@ -132,33 +132,33 @@ class Persistent(object):
     @staticmethod
     def disconnect():
         """Disconnect from database."""
-        
-        Persistent.connection.disconnect()
+
+        Persistent.connection.close()
         Persistent.database = None
 
     def load(self, obj):
         """Load object literal in object."""
         self.inner = obj
-    
+
     @classmethod
     def exists(cls, obj):
         """Checks if a document with the specified fields exists in the database."""
-        
+
         database = Persistent.database
-        res =  database[cls.collection()].find_one(obj) 
+        res =  database[cls.collection()].find_one(obj)
         return res != None
-    
+
     def get_id(self):
         """Get object id of persistent object."""
-        
+
         if "_id" not in self.inner:
             self.save()
-            
+
         return self["_id"]
-        
+
     @staticmethod
     def run_and_report(cmd):
-        
+
         """Run command on the shell, report stdout, stderr"""
         proc = Persistent.run(cmd)
         proc.wait()
@@ -169,11 +169,11 @@ class Persistent(object):
     def run(cmd):
         """Run """
         return Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, close_fds=True) # close_fds doesn't seem to work properly
-        
+
     @staticmethod
     def revision():
         """Get scm revision."""
-        
+
         if Persistent.scm == "git":
             return Persistent.run_and_report("git log -n 1 | grep commit | sed -e \"s/commit //\"", None)
         elif Persistent.scm == "mercurial":
@@ -183,17 +183,17 @@ class Persistent(object):
     @staticmethod
     def host():
         """Get current machine hostname."""
-        
+
         return Persistent.run_and_report("hostname")
-        
+
     @staticmethod
     def user():
         """Get current user."""
-        
+
         return Persistent.run_and_report("whoami")
-        
+
     @staticmethod
     def platform():
         """Get current platform."""
-        
+
         return Persistent.run_and_report("uname -sr")
